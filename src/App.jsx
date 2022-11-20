947e2cc4511510732df8e5d5dc0289f91da48b92
raihanhn <raihanhn20@gmail.com>
2022-11-20 22:56:49 +0600

    1st

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbarku from './components/Navbarku';
+
+
+function App() {
+
+  return (
+    <div className="App">
+
+     <Navbarku/>
+   
+    </div>
+  )
+}
+
+export default App
