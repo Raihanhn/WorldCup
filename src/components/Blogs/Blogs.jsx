d95cccb389ee0dfc76fc7b5d832fb85c8edfb5ed
raihanhn <raihanhn20@gmail.com>
2022-11-24 03:26:08 +0600

    8th

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Blogs = () => {
+  const [active,setActive] = useState(false);
+  const input= React.useRef();
+  useEffect(() =>{
+     if(active) input.current.focus();
+  }, [active] );
+
+
   return (
     <div className='blogss'>
-        Blogging is not a easy task so.
+        hello world
+        <div className="container">
+        <div className="searchbar">
+          <input type="search" placeholder='search here...'  ref={input} className={active?'input active':'input'} />
+          <button className='btn' onClick={()=>setActive(!active)}>BT</button>
+        </div>
+        </div>
     </div>
   )
 }
--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Review = () => {
+
+
+  const [active,setActive] = useState(false);
+  const input= React.useRef();
+  useEffect(() =>{
+     if(active) input.current.focus();
+  }, [active] );
+
+
+
+
   return (
     <div className='reviews'>
         Argentina hari gece
+        <div className="containers">
+        <div className="searchbars">
+          <input type="search" placeholder='search here...'  ref={input} className={active?'input active':'input'} />
+        </div>
+        <button className='btns' onClick={()=>setActive(!active)}>BT</button>
+        </div>
     </div>
   )
 }
