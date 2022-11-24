9ea39b33bd7fce252e545286bba7700b77613db5
raihanhn <raihanhn20@gmail.com>
2022-11-24 17:47:37 +0600

    8th

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import { useEffect, useState } from 'react'
+import { BsSearch } from 'react-icons/bs';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 const Header = () => {
+
+  const [active,setActive] = useState(false);
+  const input= React.useRef();
+  useEffect(() =>{
+     if(active) input.current.focus();
+  }, [active] );
+
   return (
     <header className="header">
     <a href="#" className='logo'> World Cup </a>
-
     <nav className="navbar">
         <ul>
         <li><Link className='right' to="/">home</Link></li>
@@ -16,6 +25,16 @@ const Header = () => {
         <li><Link className='right' to="/blogs">blogs</Link></li>
         </ul>
     </nav>
+
+
+    <div className="containers">
+        <div className="searchbars">
+          <input type="search" placeholder='search here...' ref={input} className={active?'input active':'input'} />
+        </div>
+        <button className='btns' onClick={()=>setActive(!active)}> <BsSearch/></button>
+
+        <button className='btnss'> <GiHamburgerMenu/> </button>
+        </div>
     
 </header>
   )
--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,26 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 const Review = () => {
-
-
-  const [active,setActive] = useState(false);
-  const input= React.useRef();
-  useEffect(() =>{
-     if(active) input.current.focus();
-  }, [active] );
-
-
-
 
   return (
     <div className='reviews'>
         Argentina hari gece
-        <div className="containers">
-        <div className="searchbars">
-          <input type="search" placeholder='search here...'  ref={input} className={active?'input active':'input'} />
-        </div>
-        <button className='btns' onClick={()=>setActive(!active)}>BT</button>
-        </div>
     </div>
   )
 }
