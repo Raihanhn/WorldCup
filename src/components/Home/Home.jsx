27854af95d7d25f979b00363721d96076c44c5fe
raihanhn <raihanhn20@gmail.com>
2022-11-27 18:53:54 +0600

    12th

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='homes'>
-      ami hocci home bujcho??
-    </div>
+    <section className="home">
+      <h3>Qatar <span>world cup</span> 2022</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptatum quis, cupiditate eius fuga unde asperiores assumenda perspiciatis. Aliquam, hic!</p>
+      <a href="#" className='btn'>Read more</a>
+    </section>
   )
 }
 
