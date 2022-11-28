378a6a21a28efe24b8e96347d39f13493fd1e675
raihanhn <raihanhn20@gmail.com>
2022-11-28 15:45:23 +0600

    13th

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 const Home = () => {
   return (
     <section className="home">
+      <div className="content">
       <h3>Qatar <span>world cup</span> 2022</h3>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptatum quis, cupiditate eius fuga unde asperiores assumenda perspiciatis. Aliquam, hic!</p>
-      <a href="#" className='btn'>Read more</a>
+      <a href="#" className='mybtn'>Read more</a>
+      </div>
     </section>
   )
 }
