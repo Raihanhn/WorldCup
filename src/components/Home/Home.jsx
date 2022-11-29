e527983d57e7ac4b398f572d97564928d2af7a41
raihanhn <raihanhn20@gmail.com>
2022-11-29 13:18:47 +0600

    15th

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import img1 from "../../assets/img/pic-1.png"
+import img2 from "../../assets/img/pic-2.png"
+import img3 from "../../assets/img/pic-3.png"
 
 const Home = () => {
   return (
+    <div className="akeel">
     <section className="home">
       <div className="content">
       <h3>Qatar <span>world cup</span> 2022</h3>
@@ -9,6 +13,38 @@ const Home = () => {
       <a href="#" className='mybtn'>Read more</a>
       </div>
     </section>
+
+    <section className="countries">
+      <h1 className="heading">worldcup <span>countries</span></h1>
+
+      <div className="box-container">
+
+        <div className="box">
+          <img src={img1} alt="" />
+          <h3>fresh and organic</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, debitis!</p>
+          <a href="#" className='mybtn'>read more</a>
+        </div>
+
+        <div className="box">
+          <img src={img2} alt="" />
+          <h3>fresh and organic</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, debitis!</p>
+          <a href="#" className='mybtn'>read more</a>
+        </div>
+
+        <div className="box">
+          <img src={img3} alt="" />
+          <h3>fresh and organic</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, debitis!</p>
+          <a href="#" className='mybtn'>read more</a>
+        </div>
+
+      </div>
+    </section>
+
+
+    </div>
   )
 }
 
