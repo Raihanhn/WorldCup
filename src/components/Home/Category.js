109ca7592095cab7894efd74d44eb798370fedb0
raihanhn <raihanhn20@gmail.com>
2022-11-30 17:53:20 +0600

    19th

--- src/components/Home/Category.js
+++ src/components/Home/Category.js
@@ -3,149 +3,261 @@ import img2 from '../../assets/img/ecuador.jpg'
 import img3 from '../../assets/img/senegal.jpeg'
 import img4 from '../../assets/img/nedarland.jpg'
 import img5 from '../../assets/img/england.jpg'
+import img6 from '../../assets/img/iran.jpg'
+import img7 from '../../assets/img/usa.jpg'
+import img8 from '../../assets/img/wales.jpg'
+import img9 from '../../assets/img/argentina.jpg'
+import img10 from '../../assets/img/sauiarabia.jpg'
+import img11 from '../../assets/img/mexico.jpg'
+import img12 from '../../assets/img/poland.png'
+import img13 from '../../assets/img/france.jpg'
+import img14 from '../../assets/img/australia.jpg'
+import img15 from '../../assets/img/denmark.jpeg'
+import img16 from '../../assets/img/tunisia.jpeg'
+import img17 from '../../assets/img/spain.jpg'
+import img18 from '../../assets/img/costa rica.jpg'
+import img19 from '../../assets/img/germany.jpg'
+import img20 from '../../assets/img/japan.jpg'
+import img21 from '../../assets/img/belgium.jpg'
+import img22 from '../../assets/img/canada.jpg'
+import img23 from '../../assets/img/morocco.jpg'
+import img24 from '../../assets/img/croatia.jpg'
+import img25 from '../../assets/img/brazil.jpg'
+import img26 from '../../assets/img/serbia.jpg'
+import img27 from '../../assets/img/switzerland.jpg'
+import img28 from '../../assets/img/cameroon.jpg'
+import img29 from '../../assets/img/portugal.jpg'
+import img30 from '../../assets/img/ghana.jpeg'
+import img31 from '../../assets/img/uruguay.jpg'
+import img32 from '../../assets/img/korea republic.jpg'
 
 
 const Categories = [
     {
         id:1,
         title:"qatar Team",
-        desc:"1930",
-        bttn :"Uruguay",
+        desc:"fifa ranking: 50",
+        bttn :"read more",
         image:img1
     },
     {
         id:2,
         title:"ecuador Team",
-        desc:"1930",
-        bttn :"Uruguay",
+        desc:"fifa ranking: 44",
+        bttn :"read more",
         image:img2
     },
     {
         id:3,
         title:"senegal Team",
-        desc:"1930",
-        bttn :"Uruguay",
+        desc:"fifa ranking: 18",
+        bttn :"read more",
         image:img3
     },
     {
         id:4,
         title:"nedarland Team",
-        desc:"1930",
-        bttn :"Uruguay",
+        desc:"fifa ranking: 8",
+        bttn :"read more",
         image:img4
     },
     {
         id:5,
         title:"england Team",
-        desc:"1930",
-        bttn :"Uruguay",
+        desc:"fifa ranking: 5",
+        bttn :"read more",
         image:img5
+    },
+    {
+        id:6,
+        title:"iran Team",
+        desc:"fifa ranking: 20",
+        bttn :"read more",
+        image:img6
+    },
+    {
+        id:7,
+        title:"uSA Team",
+        desc:"fifa ranking: 16",
+        bttn :"read more",
+        image:img7
+    },
+    {
+        id:8,
+        title:"wales Team",
+        desc:"fifa ranking: 19",
+        bttn :"read more",
+        image:img8
+    },
+    {
+        id:9,
+        title:"argentina Team",
+        desc:"fifa ranking: 3",
+        bttn :"read more",
+        image:img9
+    },
+    {
+        id:10,
+        title:"saudi arabia Team",
+        desc:"fifa ranking: 51",
+        bttn :"read more",
+        image:img10
+    },
+    {
+        id:11,
+        title:"mexico Team",
+        desc:"fifa ranking: 13",
+        bttn :"read more",
+        image:img11
+    },
+    {
+        id:12,
+        title:"poland Team",
+        desc:"fifa ranking: 26",
+        bttn :"read more",
+        image:img12
+    },
+    {
+        id:13,
+        title:"france Team",
+        desc:"fifa ranking: 4",
+        bttn :"read more",
+        image:img13
+    },
+    {
+        id:14,
+        title:"australia Team",
+        desc:"fifa ranking: 38",
+        bttn :"read more",
+        image:img14
+    },
+    {
+        id:15,
+        title:"denmark Team",
+        desc:"fifa ranking: 10",
+        bttn :"read more",
+        image:img15
+    },
+    {
+        id:16,
+        title:"tunisia Team",
+        desc:"fifa ranking: 30",
+        bttn :"read more",
+        image:img16
+    },
+    {
+        id:17,
+        title:"spain Team",
+        desc:"fifa ranking: 7",
+        bttn :"read more",
+        image:img17
+    },
+    {
+        id:18,
+        title:"costa rica Team",
+        desc:"fifa ranking: 31",
+        bttn :"read more",
+        image:img18
+    },
+    {
+        id:19,
+        title:"germany Team",
+        desc:"fifa ranking: 11",
+        bttn :"read more",
+        image:img19
+    },
+    {
+        id:20,
+        title:"japan Team",
+        desc:"fifa ranking: 24",
+        bttn :"read more",
+        image:img20
+    },
+    {
+        id:21,
+        title:"belgium Team",
+        desc:"fifa ranking: 2",
+        bttn :"read more",
+        image:img21
+    },
+    {
+        id:22,
+        title:"canada Team",
+        desc:"fifa ranking: 41",
+        bttn :"read more",
+        image:img22
+    },
+    {
+        id:23,
+        title:"morocco Team",
+        desc:"fifa ranking: 22",
+        bttn :"read more",
+        image:img23
+    },
+    {
+        id:24,
+        title:"croatia Team",
+        desc:"fifa ranking: 12",
+        bttn :"read more",
+        image:img24
+    },
+    {
+        id:25,
+        title:"brazil Team",
+        desc:"fifa ranking: 1",
+        bttn :"read more",
+        image:img25
+    },
+    {
+        id:26,
+        title:"serbia Team",
+        desc:"fifa ranking: 21",
+        bttn :"read more",
+        image:img26
+    },
+    {
+        id:27,
+        title:"switzerland Team",
+        desc:"fifa ranking: 15",
+        bttn :"read more",
+        image:img27
+    },
+    {
+        id:28,
+        title:"cameroon Team",
+        desc:"fifa ranking: 43",
+        bttn :"read more",
+        image:img28
+    },
+    {
+        id:29,
+        title:"portugal Team",
+        desc:"fifa ranking: 9",
+        bttn :"read more",
+        image:img29
+    },
+    {
+        id:30,
+        title:"ghana Team",
+        desc:"fifa ranking: 61",
+        bttn :"read more",
+        image:img30
+    },
+    {
+        id:31,
+        title:"uruguay Team",
+        desc:"fifa ranking: 14",
+        bttn :"read more",
+        image:img31
+    },
+    {
+        id:32,
+        title:"korea republic Team",
+        desc:"fifa ranking: 28",
+        bttn :"read more",
+        image:img32
     }
-    // {
-    //     id:6,
-    //     title:"Brazil Team",
-    //     year:"1958",
-    //     venue:"Sweden",
-    //     image:img6
-    // },
-    // {
-    //     id:7,
-    //     title:"Brazil Team",
-    //     year:"1962",
-    //     venue:"Chile",
-    //     image:img7
-    // },
-    // {
-    //     id:8,
-    //     title:"Brazil Team",
-    //     year:"1966",
-    //     venue:"England",
-    //     image:img8
-    // },
-    // {
-    //     id:9,
-    //     title:"Brazil Team",
-    //     year:"1970",
-    //     venue:"Mexico",
-    //     image:img9
-    // },
-    // {
-    //     id:10,
-    //     title:"Brazil Team",
-    //     year:"1974",
-    //     venue:"Germany",
-    //     image:img10
-    // },
-    // {
-    //     id:11,
-    //     title:"Brazil Team",
-    //     year:"1978",
-    //     venue:"Argentina",
-    //     image:img11
-    // },
-    // {
-    //     id:12,
-    //     title:"Brazil Team",
-    //     year:"1982",
-    //     venue:"Spain",
-    //     image:img12
-    // },
-    // {
-    //     id:13,
-    //     title:"Brazil Team",
-    //     year:"1986",
-    //     venue:"Mexico",
-    //     image:img13
-    // },
-    // {
-    //     id:14,
-    //     title:"Brazil Team",
-    //     year:"1990",
-    //     venue:"Italy",
-    //     image:img14
-    // },
-    // {
-    //     id:15,
-    //     title:"Brazil Team",
-    //     year:"1994",
-    //     venue:"United States",
-    //     image:img15
-    // },
-    // {
-    //     id:16,
-    //     title:"Brazil Team",
-    //     year:"1998",
-    //     venue:"France",
-    //     image:img16
-    // },
-    // {
-    //     id:17,
-    //     title:"Brazil Team",
-    //     year:"2002",
-    //     venue:"Japan",
-    //     image:img17
-    // },
-    // {
-    //     id:18,
-    //     title:"Brazil Team",
-    //     year:"2006",
-    //     venue:"Germany",
-    //     image:img18
-    // },
-    // {
-    //     id:19,
-    //     title:"Brazil Team",
-    //     year:"2010",
-    //     venue:"South Africa",
-    //     image:img19
-    // },
-    // {
-    //     id:20,
-    //     title:"Brazil Team",
-    //     year:"2014",
-    //     venue:"Brazil",
-    //     image:img20
-    // }
+   
  ]
 
  export default Categories;
