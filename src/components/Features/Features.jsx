e554515c20ad5c979c341f78500c0bcb8f91df5f
raihanhn <raihanhn20@gmail.com>
2022-12-03 14:53:43 +0600

    27th

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import img from "../../assets/img/qatarflag.jpg"
+import img from "../../assets/img/hard.jpg"
 import img1 from "../../assets/img/groups.jpg"
+import img2 from "../../assets/img/top16.png"
 
 const Features = () => {
   return (
@@ -12,6 +13,15 @@ const Features = () => {
       <div className="box-container">
         <div className="boxs">
            <img src={img1} alt="" />
+        </div>
+      </div>
+    </section>
+
+    <section className='feature-card'>
+      <h1 className="heading">top<span>sixteen</span>teams</h1>
+      <div className="box-container">
+        <div className="boxs">
+           <img src={img2} alt="" />
         </div>
       </div>
     </section>
