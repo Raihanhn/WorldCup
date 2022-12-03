3320d6825539ee35e53ba0ef5049db84614281d8
raihanhn <raihanhn20@gmail.com>
2022-12-03 14:29:16 +0600

    26th

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import img from "../../assets/img/qatarflag.jpg"
+import img1 from "../../assets/img/groups.jpg"
 
 const Features = () => {
   return (
@@ -7,10 +8,10 @@ const Features = () => {
       <img src={img} alt="" className='qatarflag' />
 
     <section className='features'>
-
+    <h1 className="heading">qatar<span>2022</span>groups</h1>
       <div className="box-container">
         <div className="boxs">
-        
+           <img src={img1} alt="" />
         </div>
       </div>
     </section>
