142045d2394fd4455283a99271eae14fec2c2136
raihanhn <raihanhn20@gmail.com>
2022-12-05 06:19:46 +0600

    29th

---
+++ src/components/Products/Product.js
@@ -0,0 +1,39 @@
+import img1 from "../../assets/products/p1.jpg"
+import img2 from "../../assets/products/p2.jpg"
+import img3 from "../../assets/products/p3.jpg"
+import img4 from "../../assets/products/p4.jpg"
+
+
+const Product = [
+    {
+        id:1,
+        title:"qatar Team",
+        desc:"fifa ranking: 50",
+        bttn :"shop now",
+        image:img1
+    },
+    {
+        id:2,
+        title:"ecuador Team",
+        desc:"fifa ranking: 44",
+        bttn :"shop now",
+        image:img2
+    },
+    {
+        id:3,
+        title:"senegal Team",
+        desc:"fifa ranking: 18",
+        bttn :"shop now",
+        image:img3
+    },
+    {
+        id:4,
+        title:"nedarland Team",
+        desc:"fifa ranking: 8",
+        bttn :"read more",
+        image:img4
+    }
+   
+ ]
+
+ export default Product;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
+import product from "./Product"
 import img from "../../assets/img/ator.jpg"
+import { useState } from 'react'
 
 const Products = () => {
+
+  const [data, setData] = useState(product);
+
   return (
     <div className='tough'>
-         <img src={img} alt="" className='qatarflags' />
+      <img src={img} alt="" className='qatarflags' />
+
+        <section className="products">
+         <h1 className="heading">product<span>categories</span></h1>
+
+         <div className="box-container">
+
+      {data.map((value) =>{
+                const {id, title, desc, bttn, image} = value;
+
+                return (
+                    <>
+                        <div className="box" key={id}>
+                          <img src={image} alt="" />
+                         <h3> {title} </h3>
+                          <p>{desc}</p>
+                          <a href="#" className='mybtns'> {bttn} </a>
+                        </div>
+                    </>
+                )
+            })}
+
+         </div>
+
+        </section>
 
     </div>
   )
