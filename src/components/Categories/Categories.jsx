d576d0c14cc83bd70af4bec9b23c0fecff79c06d
raihanhn <raihanhn20@gmail.com>
2022-12-08 22:25:11 +0600

    36th

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { useState } from 'react'
 import img20 from "../../assets/img/catary.jpg"
 import jersey from "./jersey"
+import phant from "./phant"
 
 const Categories = () => {
 
   const [data, setData] = useState(jersey);
+  const [datta, setDatta] = useState(phant);
 
 
   return (
@@ -35,6 +37,32 @@ const Categories = () => {
          </div>
 
       </section>
+
+      <section className="categories">
+          <h1 className="heading">football<span>phant</span></h1>
+
+          <div className="box-container">
+
+         {datta.map((value) =>{
+            const {id, title, desc, bttn, image} = value;
+
+          return (
+              <>
+                  <div className="box" key={id}>
+                    <img src={image} alt="" />
+                   <h3> {title} </h3>
+                    <p>{desc}</p>
+                    <a href="#" className='mybtns'> {bttn} </a>
+                  </div>
+              </>
+            )
+          })}
+
+         </div>
+
+      </section>
+
+
     </div>
   )
 }
---
+++ src/components/Categories/phant.js
@@ -0,0 +1,105 @@
+import img1 from "../../assets/phant/n1.jpg"
+import img2 from "../../assets/phant/n2.jpg"
+import img3 from "../../assets/phant/n3.jpg"
+import img4 from "../../assets/phant/n4.jpg"
+import img5 from "../../assets/phant/n5.png"
+import img6 from "../../assets/phant/n6.jpg"
+import img7 from "../../assets/phant/n7.jpg"
+import img8 from "../../assets/phant/n8.jpg"
+import img9 from "../../assets/phant/n9.jpg"
+import img10 from "../../assets/phant/n10.jpg"
+import img11 from "../../assets/phant/n11.png"
+import img12 from "../../assets/phant/n12.jpg"
+
+
+
+
+const phant = [
+    {
+        id:1,
+        title:"football phant",
+        desc:"upto 25% off",
+        bttn :"shop now",
+        image:img1
+    },
+    {
+        id:2,
+        title:"football phant",
+        desc:"upto 25% off",
+        bttn :"shop now",
+        image:img2
+    },
+    {
+        id:3,
+        title:"football phant",
+        desc:"upto 25% off",
+        bttn :"shop now",
+        image:img3
+    },
+    {
+        id:4,
+        title:"football phant",
+        desc:"upto 25% off",
+        bttn :"shop now",
+        image:img4
+    },
+    {
+        id:5,
+        title:"football phant",
+        desc:"upto 25% off",
+        bttn :"shop now",
+        image:img5
+    },
+    {
+        id:6,
+        title:"football phant",
+        desc:"upto 25% off",
+        bttn :"shop now",
+        image:img6
+    },
+    {
+        id:7,
+        title:"football phant",
+        desc:"upto 25% off",
+        bttn :"shop now",
+        image:img7
+    },
+    {
+        id:8,
+        title:"football phant",
+        desc:"upto 25% off",
+        bttn :"shop now",
+        image:img8
+    },
+    {
+        id:9,
+        title:"football phant",
+        desc:"upto 25% off",
+        bttn :"shop now",
+        image:img9
+    },
+    {
+        id:10,
+        title:"football phant",
+        desc:"upto 25% off",
+        bttn :"shop now",
+        image:img10
+    },
+    {
+        id:11,
+        title:"football phant",
+        desc:"upto 25% off",
+        bttn :"shop now",
+        image:img11
+    },
+    {
+        id:12,
+        title:"football phant",
+        desc:"upto 25% off",
+        bttn :"shop now",
+        image:img12
+    }
+   
+ ]
+
+ export default phant;
