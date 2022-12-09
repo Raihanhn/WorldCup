8475984eb5e35b9848223eb5e5631746e7bd1e33
raihanhn <raihanhn20@gmail.com>
2022-12-09 19:44:39 +0600

    38th

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -4,12 +4,14 @@ import img20 from "../../assets/img/catary.jpg"
 import jersey from "./jersey"
 import phant from "./phant"
 import socks from "./socks"
+import boots from "./boots"
 
 const Categories = () => {
 
   const [data, setData] = useState(jersey);
   const [datta, setDatta] = useState(phant);
   const [dataa, setDataa] = useState(socks);
+  const [daata, setDaata] = useState(boots);
 
 
   return (
@@ -70,6 +72,31 @@ const Categories = () => {
           <div className="box-container">
 
          {dataa.map((value) =>{
+            const {id, title, desc, bttn, image} = value;
+
+          return (
+              <>
+                  <div className="box" key={id}>
+                    <img src={image} alt="" />
+                   <h3> {title} </h3>
+                    <p>{desc}</p>
+                    <a href="#" className='mybtns'> {bttn} </a>
+                  </div>
+              </>
+            )
+          })}
+
+         </div>
+
+      </section>
+
+      
+      <section className="categories">
+          <h1 className="heading">football<span>socks</span></h1>
+
+          <div className="box-container">
+
+         {daata.map((value) =>{
             const {id, title, desc, bttn, image} = value;
 
           return (
--- src/components/Categories/boots.js
+++ src/components/Categories/boots.js
@@ -1,100 +1,100 @@
-import img1 from "../../assets/socks/s1.jpg"
-import img2 from "../../assets/socks/s2.jpg"
-import img3 from "../../assets/socks/s3.jpg"
-import img4 from "../../assets/socks/s4.jpg"
-import img5 from "../../assets/socks/s5.jpg"
-import img6 from "../../assets/socks/s6.jpg"
-import img7 from "../../assets/socks/s7.jpg"
-import img8 from "../../assets/socks/s8.jpg"
-import img9 from "../../assets/socks/s9.jpg"
-import img10 from "../../assets/socks/s10.jpg"
-import img11 from "../../assets/socks/s11.jpg"
-import img12 from "../../assets/socks/s12.jpg"
+import img1 from "../../assets/boots/b1.jpg"
+import img2 from "../../assets/boots/b2.jpg"
+import img3 from "../../assets/boots/b3.jpg"
+import img4 from "../../assets/boots/b4.jpg"
+import img5 from "../../assets/boots/b5.jpg"
+import img6 from "../../assets/boots/b6.jpg"
+import img7 from "../../assets/boots/b7.jpg"
+import img8 from "../../assets/boots/b8.jpg"
+import img9 from "../../assets/boots/b9.jpg"
+import img10 from "../../assets/boots/b10.jpg"
+import img11 from "../../assets/boots/b11.jpg"
+import img12 from "../../assets/boots/b12.jpg"
 
 
 
 
-const socks = [
+const boots = [
     {
         id:1,
-        title:"football socks",
+        title:"football boots",
         desc:"upto 25% off",
         bttn :"shop now",
         image:img1
     },
     {
         id:2,
-        title:"football socks",
+        title:"football boots",
         desc:"upto 25% off",
         bttn :"shop now",
         image:img2
     },
     {
         id:3,
-        title:"football socks",
+        title:"football boots",
         desc:"upto 25% off",
         bttn :"shop now",
         image:img3
     },
     {
         id:4,
-        title:"football socks",
+        title:"football boots",
         desc:"upto 25% off",
         bttn :"shop now",
         image:img4
     },
     {
         id:5,
-        title:"football socks",
+        title:"football boots",
         desc:"upto 25% off",
         bttn :"shop now",
         image:img5
     },
     {
         id:6,
-        title:"football socks",
+        title:"football boots",
         desc:"upto 25% off",
         bttn :"shop now",
         image:img6
     },
     {
         id:7,
-        title:"football socks",
+        title:"football boots",
         desc:"upto 25% off",
         bttn :"shop now",
         image:img7
     },
     {
         id:8,
-        title:"football socks",
+        title:"football boots",
         desc:"upto 25% off",
         bttn :"shop now",
         image:img8
     },
     {
         id:9,
-        title:"football socks",
+        title:"football boots",
         desc:"upto 25% off",
         bttn :"shop now",
         image:img9
     },
     {
         id:10,
-        title:"football socks",
+        title:"football boots",
         desc:"upto 25% off",
         bttn :"shop now",
         image:img10
     },
     {
         id:11,
-        title:"football socks",
+        title:"football boots",
         desc:"upto 25% off",
         bttn :"shop now",
         image:img11
     },
     {
         id:12,
-        title:"football socks",
+        title:"football boots",
         desc:"upto 25% off",
         bttn :"shop now",
         image:img12
@@ -102,4 +102,4 @@ const socks = [
    
  ]
 
- export default socks;
+ export default boots;
