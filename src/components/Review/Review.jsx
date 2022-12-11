6c6195d4ccef327e7495166e1753f00ead75009e
raihanhn <raihanhn20@gmail.com>
2022-12-11 17:39:42 +0600

    41th

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { AiFillStar } from 'react-icons/ai';
+import img1 from "../../assets/review/rs-5.png"
 
 const Review = () => {
 
@@ -7,12 +9,20 @@ const Review = () => {
        <section className="reviews">
         <h1 className="heading">customer <span>reviews</span></h1>
         <div className="box-container">
-          world cup
+          <div className="box">
+            <img src={img1} alt="" />
+            <h3>john deo</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos sunt natus ipsam nesciunt, eos quisquam ut reiciendis nostrum iure</p>
+            <div className="stars"> <AiFillStar/> <AiFillStar/> <AiFillStar/> <AiFillStar/> <AiFillStar/> </div>
+    
+          </div>
+
+          <div className="box">
+            i am roky
+          </div>
+
         </div>
 
-        <div className="box-container">
-          world cup
-        </div>
 
         
        </section>
