2720656ec4b7fe354b0d7ba3ba320d239e400552
raihanhn <raihanhn20@gmail.com>
2022-12-11 18:51:28 +0600

    43th

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import img from "../../assets/img/porto.jpg"
 
 const Blogs = () => {
   const [active,setActive] = useState(false);
@@ -9,14 +10,17 @@ const Blogs = () => {
 
 
   return (
-    <div className='blogss'>
-        hello world
+    <div className='blog'>
+      <img src={img} alt="" className='blogimg' />
+       <section className="blogs">
+         <h1 className='topline'> hello world welcome to qatar</h1>
         <div className="container">
         <div className="searchbar">
           <input type="search" placeholder='search here...'  ref={input} className={active?'input active':'input'} />
           <button className='btn' onClick={()=>setActive(!active)}>BT</button>
         </div>
         </div>
+       </section>
     </div>
   )
 }
