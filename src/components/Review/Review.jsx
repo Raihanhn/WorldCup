c6ee3c602b41a5f737d9eaa09fe670c8eb8387e4
raihanhn <raihanhn20@gmail.com>
2022-12-11 17:57:26 +0600

    42th

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { AiFillStar } from 'react-icons/ai';
 import img1 from "../../assets/review/rs-5.png"
+import img2 from "../../assets/review/rs-6.png"
+import img4 from "../../assets/review/rs-7.png"
+import img3 from "../../assets/review/rs-4.png"
 
 const Review = () => {
 
@@ -18,14 +21,33 @@ const Review = () => {
           </div>
 
           <div className="box">
-            i am roky
+            <img src={img2} alt="" />
+            <h3>jasica oliver</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos sunt natus ipsam nesciunt, eos quisquam ut reiciendis nostrum iure</p>
+            <div className="stars"> <AiFillStar/> <AiFillStar/> <AiFillStar/> <AiFillStar/> <AiFillStar/> </div>
+          </div>
+        </div>
+
+        <div className="box-container">
+          <div className="box">
+            <img src={img3} alt="" />
+            <h3>alisa margarate</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos sunt natus ipsam nesciunt, eos quisquam ut reiciendis nostrum iure</p>
+            <div className="stars"> <AiFillStar/> <AiFillStar/> <AiFillStar/> <AiFillStar/> <AiFillStar/> </div>
+    
           </div>
 
+          <div className="box">
+            <img src={img4} alt="" />
+            <h3>alison banz</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos sunt natus ipsam nesciunt, eos quisquam ut reiciendis nostrum iure</p>
+            <div className="stars"> <AiFillStar/> <AiFillStar/> <AiFillStar/> <AiFillStar/> <AiFillStar/> </div>
+          </div>
         </div>
 
 
-        
        </section>
+
     </div>
   )
 }
